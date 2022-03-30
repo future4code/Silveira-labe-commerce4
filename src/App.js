6c3b0d96373636b0874716e304113ea669de2753
Alessandra-Marmiroli <alessandramarmiroli@gmail.com>
2022-03-30 15:20:31 -0300

    labe-ecommerce

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  state = {
+    homePage: [{
+    valorMínimo: "",
+    valorMáximo: "", 
+    BuscaPorNome: "",
+    ordenacao: "Crescente",
+    
+    }],
+    carrinho: [{}]
+  }
 }
+render(){
+  return(
+    <div>
+      <input placeholder='' value={this.state.valorMínimo}/>
+      <input placeholder='' value={this.state.valorMáximo}/>
+    </div>
+  ),
+},
 
-export default App;
+
