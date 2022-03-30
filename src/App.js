2d025470298b3d44b3c31f0f89dbb0d3d93b4ef7
Baulii <adernam147@hotmail.com>
2022-03-30 16:27:36 -0400

    bauli

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import styled from "styled-components";
-import { render } from '@testing-library/react';
 
 
 export default class App extends React.Component {
   state = {
-    homePage: [{
-      valorMinimo: "",
-      valorMaximo: "",
-      BuscaPorNome: "",
-      ordenacao: "Crescente",
 
-    }],
-    carrinho: [{}]
   }
-}
-render(){
-  return (
-    <div>
-      <input placeholder='' value={this.state.valorMinimo} />
-      <input placeholder='' value={this.state.valorMaximo} />
-    </div>
-  )
+
+  render() {
+    return (
+    )
+  }
+
 }
