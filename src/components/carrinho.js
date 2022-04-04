6796e632d5d9849a5c908e00a206bb4d4bdbf585
brennoambrozino <brennoambrozino@gmail.com>
2022-04-03 22:25:48 -0300

    conflitos

--- src/components/carrinho.js
+++ src/components/carrinho.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
+import styled from 'styled-components'
 
-export default class carrinho extends Component {
+const CarrinhoContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    border: 1px solid black;
+    height: 100vh; 
+    width: 15vw; 
+    padding: 10px;
+    margin-bottom: 20px;
+`
+export default class Carrinho extends React.Component {
   render() {
     return (
-      <div>carrinho</div>
+        <CarrinhoContainer/>
     )
   }
 }
